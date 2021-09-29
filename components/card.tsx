23036d846149ac8c7829534638df270da61cd505
Joseph Bond <jcbond92@gmail.com>
2021-09-29 17:57:57 -0400

    feat: integrate api

--- components/card.tsx
+++ components/card.tsx
@@ -1,14 +1,20 @@
-const Card = () => {
+const Card = (props:any) => {
   return (
-    <a
-      href="https://nextjs.org/docs"
-      className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+    <div
+      className="p-6 mt-6 text-left border w-96 rounded-xl relative"
     >
-      <h3 className="text-2xl font-bold">Documentation</h3>
-      <p className="mt-4 text-xl">
-        Find in-depth information about Next.js features and API.
-      </p>
-    </a>
+      <h3 className="text-2xl font-bold">{props.name}</h3>
+      <h4 className="absolute top-2 right-3">{props.brewery_type}</h4>
+      <ul>
+        <li>{props.street}, {props.city}, {props.state} {props.postal_code}</li>
+        <li><a className="hover:text-blue-600 focus:text-blue-600" href={props.website_url}>
+          Website
+          </a></li>
+        <li><a className="hover:text-blue-600 focus:text-blue-600" target="_blank" href={`https://www.google.com/maps/search/?api=1&query=${props.obdb_id}`}>
+          Google Maps
+          </a></li>
+      </ul>
+    </div>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,11 @@
-import SiteWrapper from "../components/site-wrapper.tsx";
-import Card from "../components/card.tsx";
-import CardContainer from "../components/card-container.tsx";
+import SiteWrapper from "@/components/site-wrapper.tsx";
+import SearchBar from "@/components/search-bar.tsx";
 
 export default function Home() {
   return (
     <SiteWrapper>
       <p className="mt-3 text-2xl">Get started by searching for a brewery </p>
-      <CardContainer>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </CardContainer>
+      <SearchBar />
     </SiteWrapper>
   );
 }
